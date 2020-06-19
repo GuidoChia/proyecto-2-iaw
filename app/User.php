63de8560c444ca9364b9f598f529cb8570286d42
GuidoChia <chiaguido@gmail.com>
2020-06-19 18:17:55 -0300

    Agrego api. Api con register, login, y dos metodos para pedir informacion.

--- app/User.php
+++ app/User.php
@@ -2,13 +2,13 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
     const ADMIN_TYPE = 'admin';
     const USER_TYPE = 'user';
     public const DEFAULT_TYPE= self::USER_TYPE;
